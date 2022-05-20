9b4fbef6d35ae3fb582931b5d196e4e534802018
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2022-05-20 18:06:18 +0300

    spring boot base API created

---
+++ src/test/java/com/itechart/websocket_task2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itechart.websocket_task2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
